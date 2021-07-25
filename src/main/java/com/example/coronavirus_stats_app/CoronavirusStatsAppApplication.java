5f461a372c03f7a2b9b68d20a39246606ab05dff
Pranesh Reddy <praneshreddy2000@gmail.com>
2021-07-25 16:27:30 +0400

    Spring Boot starter code

---
+++ src/main/java/com/example/coronavirus_stats_app/CoronavirusStatsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.coronavirus_stats_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusStatsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusStatsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coronavirus_stats_app/CoronavirusStatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coronavirus_stats_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusStatsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
