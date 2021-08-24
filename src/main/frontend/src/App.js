4d64ffe14d64675f9b3715e52549b2235f864ce3
Pranesh <praneshreddy2000@gmail.com>
2021-08-24 13:35:38 +0400

    Return countries based on the user search text

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,7 +5,25 @@ import {useEffect} from 'react'
 
 function App() {
 
-  const [globalData, setGlobalData] = useState([]);
+  const [globalData, setGlobalData] = useState({globalConfirmedCases: 0, 
+    globalDeaths: 0, 
+    globalNewCases: 0, 
+    globalNewDeaths: 0, 
+    countryDataList: [
+      {
+        country: '',
+        totalCountryConfirmedCases: 0,
+        totalCountryConfirmedDeaths: 0,
+        totalCountryNewConfirmedCases: 0,
+        totalCountryNewConfirmedDeaths: 0
+      }
+    ]
+    });
+
+  const [searchText, setSearchText] = useState('');
+
+  const [filteredSearchArray, setFilteredSearchArray] = useState([]);
+                                      
 
   // Fetch data from backend
   useEffect(() => {
@@ -13,10 +31,23 @@ function App() {
     .get("http://localhost:8080/api/v1/home")
     .then(result => {
       setGlobalData(result.data)
-      console.log(globalData)
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log('lololol'));
   }, []);
+
+  // When the value in the search box is changed,
+  // useState for search text
+  const handleChange = (e) => {
+    setSearchText(e.target.value);
+  }
+
+  // Every time search text is updated, we have to filter out the
+  // country list based on the search text.
+  useEffect(() => {
+    const dataList = globalData.countryDataList
+    setFilteredSearchArray(dataList.filter(countryData => 
+      countryData.country.toLowerCase().includes(searchText.toLowerCase())));
+  }, [searchText])
 
 
   return (
@@ -28,19 +59,19 @@ function App() {
         </h1>
 
         <h2 className="totalCases">
-          Total Cases: {globalData.globalConfirmedCases.toLocaleString()}
+          Total Cases: {globalData.globalConfirmedCases.toLocaleString('en-US')}
         </h2>
 
         <h2 className="totalDeaths">
-          Total Deaths: {globalData.globalDeaths.toLocaleString()}
+          Total Deaths: {globalData.globalDeaths.toLocaleString('en-US')}
         </h2>
 
         <h2 className="newCases">
-          New Cases: {globalData.globalNewCases.toLocaleString()}
+          New Cases: {globalData.globalNewCases.toLocaleString('en-US')}
         </h2>
 
         <h2 className="newDeaths">
-          New Deaths: {globalData.globalNewDeaths.toLocaleString()}
+          New Deaths: {globalData.globalNewDeaths.toLocaleString('en-US')}
         </h2>
 
         <h3 className='searchFormHeader'>
@@ -48,8 +79,15 @@ function App() {
         </h3>
 
         <form>
-          <input type="text" placeholder='Search' className='searchForm' />
+          <input type="text" placeholder='Search' className='searchForm' onChange={handleChange} />
         </form>
+
+        {
+          // Filter out the map
+          filteredSearchArray.map(country => {
+            return <h4>{country.country}</h4>
+          })
+        }
       </div>
 
     </div>
---
+++ src/main/frontend/src/GlobalCountryData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GlobalCountryData = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default GlobalCountryData
