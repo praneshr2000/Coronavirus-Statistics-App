0b0d31c8a7486141fd3fc5c9e7a6e0d1c0eee4e4
Pranesh <praneshreddy2000@gmail.com>
2021-09-05 18:19:44 +0400

    CSS for homepage

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { Button, Container, Col, Row } from 'react-bootstrap';
+
 
 function App() {
   return(
-    <div>
-      <h1>This is home page</h1>
-      <a href='/mailing_list'>Link for mailing list</a>
-      <a href='/countries/all'> Link for COVID stats page</a>
-    </div>
+    <Container className="homeContainer" className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+      <Row className="appHeader">
+        <h1>Welcome to the Coronavirus Statistcs and Daily Updates App!</h1>
+      </Row>
+      <Row >
+        <Col className="d-grid gap-2" className='colClass'>
+          <Button href="/countries/all" size="lg" className='button'>
+            Global Coronavirus Statistics
+          </Button>
+        </Col>
+        <Col className="d-grid gap-2" className='colClass'>
+          <Button href='/mailing_list' size="lg" className='button'>
+            Register for Daily Updates
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
